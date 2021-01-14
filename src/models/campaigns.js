f310fa88c0a05e60d27162b9d1224bdaa0ad3fff
Chris Troutner <chris.troutner@gmail.com>
2021-01-13 20:51:18 -0800

    Sketched out function to generate a new address for campaigns

--- src/models/campaigns.js
+++ src/models/campaigns.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 // const config = require('../../config')
+const WalletState = require('./wallet-state')
 
 const Campaign = new mongoose.Schema({
   drops: { type: Array },
@@ -12,7 +13,23 @@ const Campaign = new mongoose.Schema({
   hasBeenPaid: { type: Boolean, default: false }
 })
 
-// Campaign.methods.getAddress
+// Query the wallet-state model to generate a new address from the HD wallet.
+Campaign.methods.getAddress = async function getAddress () {
+  try {
+    let walletState = await WalletState.find({})
+    walletState = walletState[0]
+
+    // get the mnemonic
+    // get the index for the next address, from the wallet state.
+    console.log(`hd index: ${walletState.nextAddress}`)
+    // generate the address.
+    // increase the hd index in the wallet state model
+    // save the model
+    // return the address
+  } catch (err) {
+    console.error('Error in models/campaigns.js/getAddress()')
+  }
+}
 
 // export default mongoose.model('user', User)
 module.exports = mongoose.model('campaign', Campaign)
